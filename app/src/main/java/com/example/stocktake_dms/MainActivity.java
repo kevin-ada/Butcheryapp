d88f1d03bfc9767ac105c8084c8e8c163ed68de6
Nicholas Otieno <otienonicholas02@gmail.com>
2023-11-19 19:23:41 +0300

    Signup and login pages

---
+++ app/src/main/java/com/example/stocktake_dms/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.stocktake_dms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
