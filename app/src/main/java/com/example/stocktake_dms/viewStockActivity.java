759318dcdfe6b5a8d6791c56f51007bece9d039e
Nicholas Otieno <otienonicholas02@gmail.com>
2023-11-21 22:05:03 +0300

    view stock layout

---
+++ app/src/main/java/com/example/stocktake_dms/viewStockActivity.java
@@ -0,0 +1,14 @@
+package com.example.stocktake_dms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class viewStockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_stock);
+    }
+}
